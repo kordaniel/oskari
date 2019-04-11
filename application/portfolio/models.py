b1ef9b03c94653e1816e7fe685c3db5d10e878c7
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-04-11 15:41:06 +0300

    FIX sqlite/heroku postgresql queries

--- application/portfolio/models.py
+++ application/portfolio/models.py
@@ -46,13 +46,28 @@ class Portfolio(Base):
         stmt = text("SELECT Stock.ticker, Stock.name,"
                     " Trade.date_created AS buydate, Trade.date_modified AS selldate, Trade.amount,"
                     " Trade.buyprice, Trade.sellprice,"
-                    " ROUND((Trade.sellprice - Trade.buyprice) * Trade.amount, 2) AS total_return"
+                    " ROUND(((Trade.sellprice - Trade.buyprice) * Trade.amount)::numeric, 2) AS total_return"
                     " FROM Trade, Tradestock, Stock"
                     " WHERE Trade.portfolio_id = :portfolio_id"
                     " AND Trade.sellprice IS NOT null"
                     " AND Trade.id = Tradestock.trade_id"
                     " AND Tradestock.stock_id = Stock.id"
                     ).params(portfolio_id=self.id)
+        
+        from application import os
+        if not os.environ.get("HEROKU"):
+            #using sqlite
+            stmt = text("SELECT Stock.ticker, Stock.name,"
+                        " Trade.date_created AS buydate, Trade.date_modified AS selldate, Trade.amount,"
+                        " Trade.buyprice, Trade.sellprice,"
+                        " ROUND(((Trade.sellprice - Trade.buyprice) * Trade.amount), 2) AS total_return"
+                        " FROM Trade, Tradestock, Stock"
+                        " WHERE Trade.portfolio_id = :portfolio_id"
+                        " AND Trade.sellprice IS NOT null"
+                        " AND Trade.id = Tradestock.trade_id"
+                        " AND Tradestock.stock_id = Stock.id"
+                        ).params(portfolio_id=self.id)
+        
         res = db.engine.execute(stmt)
 
         response = []
