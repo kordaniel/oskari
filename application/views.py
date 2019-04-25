4c173828b5e1fbe182bef97b46a92cd6a993ef69
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-04-25 16:33:00 +0300

    Upgrade Jinja2 2.10 -> 2.10.1 (security vulnerability in Jinja2 versions <=2.10)

--- application/views.py
+++ application/views.py
@@ -4,10 +4,4 @@ from application.auth.models import User
 
 @app.route('/')
 def index():
-    users = User.query.all()
-    for user in users:
-        print(user.name)
-        for r in user.roles:
-            print(r.name)
-        print("----------")
     return render_template('index.html')
