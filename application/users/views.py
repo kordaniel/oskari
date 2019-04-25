1891a187281aee01b1f09c5527859694b863e965
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-04-25 21:39:14 +0300

    ADD more info to users view with possibility to add/remove superuser status for all users except original administrator

--- application/users/views.py
+++ application/users/views.py
@@ -2,7 +2,7 @@ from flask import redirect, render_template, url_for
 from flask_login import current_user
 
 from application import app, db, login_manager, login_required
-from application.auth.models import User
+from application.auth.models import User, Role
 
 
 @app.route("/users", methods=["GET"])
@@ -10,7 +10,7 @@ from application.auth.models import User
 def users_index():
     return render_template("/users/all_users.html", users = User.query.all())
 
-@app.route("/user/<user_id>", methods = ["GET"])
+@app.route("/users/<user_id>", methods = ["GET"])
 @login_required()
 def user_view(user_id):    
     if not user_id.isdigit() or int(user_id) != current_user.id:
@@ -22,3 +22,24 @@ def user_view(user_id):
         return redirect(url_for("index"))
     
     return render_template("users/user.html", user = user)
+
+@app.route("/users/<user_id>", methods = ["POST"])
+@login_required(role="ADMIN")
+def user_switch_superuser_status(user_id):
+    if user_id.isdigit():
+        user = User.query.get(user_id)
+        su_role = Role.query.filter_by(superuser=True).first()
+
+        if user.is_superuser():
+            user.roles.remove(su_role)
+        else:
+            user.roles.append(su_role)
+        db.session.commit()
+    
+    return render_template("/users/all_users.html", users = User.query.all())
+
+#@app.route("/user/<user_id>", methods = ["DELETE"])
+#@login_required(role="ADMIN")
+#def delete_user(user_id):
+#    if not user_id.isdigit():
+#        return render_template("users/all_users.html", users = User.query.all())
