e8211084da436b22b2ba435b41889ecb57f17be9
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-05-02 18:38:57 +0300

    Add pagination for stocks view

--- application/stocks/views.py
+++ application/stocks/views.py
@@ -5,9 +5,10 @@ from application import app, db, login_required
 from application.stocks.models import Stock
 from application.stocks.forms import StockForm
 
-@app.route("/stocks", methods=["GET"])
-def stocks_index():
-    return render_template("stocks/list.html", stocks = Stock.query.order_by(Stock.name).all())
+@app.route("/stocks/<int:page>", methods=["GET"])
+def stocks_index(page=1):
+    per_page=10
+    return render_template("stocks/list.html", stocks = Stock.query.order_by(Stock.name).paginate(page, per_page, error_out=False))
 
 @app.route("/stocks", methods=["POST"])
 @login_required()
@@ -22,21 +23,21 @@ def stocks_create():
     db.session().add(s)
     db.session().commit()
 
-    return redirect(url_for("stocks_index"))
+    return redirect(url_for("stocks_index", page=1))
 
-@app.route("/stocks/new")
+@app.route("/stocks/new", methods=["GET"])
 @login_required()
 def stocks_form():
     return render_template("stocks/new.html", form = StockForm())
 
-@app.route("/stocks/<stock_id>", methods=["GET"])
+@app.route("/stocks/view/<stock_id>", methods=["GET"])
 def stocks_view(stock_id):
     s = Stock.query.get(stock_id)
 
     if request.method == "GET" and s is not None:
         return render_template("/stocks/stock.html", s = s, form = StockForm(obj = s))
     
-    return redirect(url_for("stocks_index"))
+    return redirect(url_for("stocks_index", page=1))
 
 @app.route("/stocks/update/<stock_id>", methods=["POST"])
 @login_required()
@@ -49,9 +50,9 @@ def stocks_update(stock_id):
     
     form.populate_obj(s)
     db.session().commit()
-    return redirect(url_for("stocks_index"))
+    return redirect(url_for("stocks_index", page=1))
 
-@app.route("/stocks/<stock_id>", methods=["DELETE"])
+@app.route("/stocks/delete/<stock_id>", methods=["DELETE"])
 @login_required(role="ADMIN")
 def stocks_delete(stock_id):
     s = Stock.query.get(stock_id)
@@ -60,4 +61,4 @@ def stocks_delete(stock_id):
         db.session().delete(s)
         db.session().commit()
 
-    return redirect(url_for("stocks_index"))
+    return redirect(url_for("stocks_index", page=1))
